bde32954734132aca2d692ad95973964a41b3042
DamzOfficiall <bind.max.1tw.2@gmail.com>
2026-01-05 01:26:41 +0700

    Update token list

--- Damz.js
+++ Damz.js
@@ -1,5 +1,6 @@
 {
-    "tokens": [
-   "8492931292:AAFyKfQ2aH39GHhzXfuCbYxJgbB5VIVCxf0"
-    ]
+  "tokens": [
+    "8492931292:AAFyKfQ2aH39GHhzXfuCbYxJgbB5VIVCxf0",
+    "8130255404:AAFIEPQulN-qtje90M7TBru-eaQ3ttIJayc"
+  ]
 }
